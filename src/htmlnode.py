e20520eb3e5d955bf7ab4059d03c8ec0889157f3
digilab00 <ra.Ouryuken@gmail.com>
2026-09-15 04:20:22 -0400

    added html nodes and leaf nodes

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -11,10 +11,29 @@ class HTMLNode:
     def props_to_html(self):
         if self.props is not None:
             props = ''
-            for key, item in self.props:
+            for key, item in self.props.items():
                 props += f' {key}={item} '
             return props
         return ''
 
     def __repr__(self):
         return f'HTMLNode(f{self.tag}, f{self.value}, f{self.children}, f{self.props})'
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag=tag, value=value, props=props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError('All leaf nodes must have a value')
+        if not self.tag:
+            return f'{self.value}'
+        if not self.props:
+            return f'<{self.tag}>{self.value}</{self.tag}>'
+        else:
+            string = ' '
+            for key, value in self.props.items():
+                string += f'{key}={value} '
+            return f'<{self.tag} {string}>{self.value}</{self.tag}>'
+            
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_not_eq(self):
@@ -17,4 +17,12 @@ class TestHTMLNode(unittest.TestCase):
         if node.children == None and node.props == None and node.tag == None and node.value == None:
             return True
         return False
-    
+
+    def test_props_to_html(self):
+        props = {'key':'value','key2':'value2'}
+        node = HTMLNode(props=props)
+        assert node.props_to_html()
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
